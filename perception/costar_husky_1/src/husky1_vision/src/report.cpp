db709b22c5aa88041de02b3d74d8c97bb39c6af8
nalindas9 <nalindas9@gmail.com>
2020-08-08 13:42:19 -0400

    Report hardcoded Artifact successfully - Score increasing

--- perception/costar_husky_1/src/husky1_vision/src/report.cpp
+++ perception/costar_husky_1/src/husky1_vision/src/report.cpp
@@ -18,6 +18,9 @@ University of Maryland, College Park
 #include <subt_communication_broker/subt_communication_client.h>
 #include <subt_ign/CommonTypes.hh>
 #include <subt_ign/protobuf/artifact.pb.h>
+#include <boost/bind.hpp>
+#include <boost/ref.hpp>
+#include <ignition/msgs.hh>
 
 void artifactCallback(const std_msgs::String::ConstPtr&);
 void BaseStationCallback(
@@ -39,15 +42,15 @@ int main(int argc, char **argv){
     // Set up communications with the base station for artifact report
     subt::CommsClient commsClient("COSTAR_HUSKY");
     commsClient.Bind(&BaseStationCallback, "COSTAR_HUSKY");
+    // found artifacts will be attempted to be sent periodically through a timer
+    ros::Timer timer = report.createTimer(ros::Duration(1.0), boost::bind(&ReportArtifacts, _1, boost::ref(commsClient)));
     ros::spin();
     return 0;
 }
 
 // Artifact topic subscriber callback
 void artifactCallback(const std_msgs::String::ConstPtr& msg){
-    ROS_INFO("Artifact detected: %s", msg->data.c_str());
-    subt::msgs::Artifact artifact;
-    artifact.set_type(static_cast<uint32_t>(subt::ArtifactType::TYPE_BACKPACK));
+    //ROS_INFO("Artifact detected: %s", msg->data.c_str());
 }
 
 // Base Station Callback
@@ -61,7 +64,9 @@ void BaseStationCallback(const std::string& srcAddress, const std::string& dstAd
     location.y = res.artifact().pose().position().y();
     location.z = res.artifact().pose().position().z();
     
-    ROS_INFO_STREAM("Artifact at location " << location.x << ", " << location.y << ", " << "location.z" << " was recieved by the base station");
+    ROS_INFO_STREAM("Artifact " << res.artifact().type() << " at location " << location.x << ", " << location.y << ", " << location.z << " was recieved by the base station\n");
+    ROS_INFO("Message from [%s] to [%s] on port [%u]:\n [%s]", srcAddress.c_str(),
+      dstAddress.c_str(), dstPort, res.DebugString().c_str());
 
 }
 
@@ -69,14 +74,20 @@ void BaseStationCallback(const std::string& srcAddress, const std::string& dstAd
 void ReportArtifacts(const ros::TimerEvent&, subt::CommsClient& commsClient){
     subt::msgs::Artifact artifact;
     // Set Artifact type and pose
+    ignition::msgs::Pose pose;
+    pose.mutable_position()->set_x(70.661);
+    pose.mutable_position()->set_y(-4.233);
+    pose.mutable_position()->set_z(0.168);
     artifact.set_type(static_cast<uint32_t>(subt::ArtifactType::TYPE_BACKPACK)); 
+    artifact.mutable_pose()->CopyFrom(pose);
     // Serialize the Artifact
-    std::string serializeData;
-    if(!artifact.SerializeToString(&serializeData)){
+    std::string serializedData;
+    if(!artifact.SerializeToString(&serializedData)){
         ROS_ERROR_STREAM("ArtifactReporter::ReportArtifact(): Error serializing message\n" << artifact.DebugString());
     }
+    //ROS_INFO_STREAM("The serialized data is " << serializedData);
     // Report the Artifact
-    commsClient.SendTo(serializeData, subt::kBaseStationName);
+    commsClient.SendTo(serializedData, subt::kBaseStationName);
 }
 
 
