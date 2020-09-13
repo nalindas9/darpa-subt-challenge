93773d78aa3916bc794a513d5063d2637088f040
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2020-09-13 19:05:57 -0400

    enhanced recovery mode
    enter recovery mode when the robot is not moving for a while

---
+++ subt_cave_costar_husky/launch/husky_explore.cpp
@@ -0,0 +1,296 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+#include <vector>
+#include <math.h>
+
+geometry_msgs::Twist vel_command;
+
+float cliff_range = 0.5;
+float prev_x = 0.0;
+float prev_y = 0.0;
+int static_counter = 0;
+std::vector<float> lidar_range = {0.0};
+std::vector<float> lidar_angle = {0.0};
+
+//*****************  VFH params *****************
+int num_sector = 180;
+float c = 1.0;
+float a = 5.0;
+float b = 1.1; // larger b allows closer obstacles  2.0
+float bin_threshold = 4.5;
+int len_threshold = 5;
+//***********************************************
+
+int RECOVERY = 0;
+
+std::vector<float> calc_magnitude()
+{
+    std::vector<float> magnitude;
+    for (int i = 0; i < lidar_range.size(); i++)
+    {
+        magnitude.push_back(c * c * (a - b * (lidar_range[i])));
+    }
+    // hole detection
+    if (cliff_range > 1.38)
+    {
+        std::cout << "hole detected" << std::endl;
+        RECOVERY = 1;
+        for (int i = 0; i < len_threshold / 2; i++)
+        {
+            magnitude[round(num_sector / 2) + i] = a;
+            magnitude[round(num_sector / 2) - i] = a;
+        }
+    }
+    else if (cliff_range < 0.35)
+    {
+        std::cout << "rock detected" << std::endl;
+        RECOVERY = 1;
+        for (int i = 0; i < len_threshold / 2; i++)
+        {
+            magnitude[round(num_sector / 2) + i] = a;
+            magnitude[round(num_sector / 2) - i] = a;
+        }
+    }
+    return magnitude;
+}
+
+std::vector<int> calc_histogram(std::vector<float> mag)
+{
+    std::vector<float> histogram;
+    std::vector<int> hist_bin;
+    int sector_interval = round(mag.size() / num_sector);
+    for (int i = 0; i < num_sector; i++)
+    {
+        float hist = 0;
+        for (int j = 0; j < sector_interval; j++)
+        {
+            hist += mag[i * sector_interval + j];
+        }
+        histogram.push_back(hist);
+    }
+    //binarization
+    for (int i = 0; i < histogram.size(); i++)
+    {
+        if (histogram[i] > bin_threshold)
+            hist_bin.push_back(1);
+        else
+            hist_bin.push_back(0);
+    }
+
+    return hist_bin;
+}
+
+int select_sector(std::vector<int> hist_bin)
+{
+    int curr_len = 0;
+    bool isValley = false;
+    std::vector<int> candidates;
+    int mid = round(hist_bin.size() / 2);
+    int valley_idx = mid;
+    int sum = 0; // count number of 0 sectors
+    // find & store all admissible valleys
+    for (int i = 0; i < hist_bin.size(); i++)
+    {
+        if (hist_bin[i] == 0 && !isValley)
+        {
+            isValley = true;
+        }
+        if ((hist_bin[i] == 1 && isValley) || (i == hist_bin.size() - 1))
+        {
+            isValley = false;
+            if (curr_len >= len_threshold)
+            {
+                candidates.push_back(i - round(curr_len / 2));
+            }
+            curr_len = 0;
+        }
+        if (isValley)
+        {
+            curr_len++;
+        }
+        sum += hist_bin[i];
+    }
+    if ((num_sector - sum) >= len_threshold)
+    {
+        // find the valley needs minimum turning
+        int dist = 1000;
+        for (int i = 0; i < candidates.size(); i++)
+        {
+            if (abs(candidates[i] - mid) < dist)
+            {
+                valley_idx = candidates[i];
+                dist = abs(candidates[i] - mid);
+            }
+        }
+    }
+    else
+    {
+        // no valley detected
+        valley_idx = -1;
+        RECOVERY = 1;
+    }
+    return valley_idx;
+}
+
+void vel_control(int valley_idx)
+{
+    float kp = 0.008; // 0.010
+    float error = valley_idx - num_sector / 2;
+    float ang_vel = kp * error;
+    // min angular velocity
+    if (ang_vel > 0 && ang_vel <= 0.1)
+        ang_vel = 0.1;
+    if (ang_vel < 0 && ang_vel >= -0.1)
+        ang_vel = -0.1;
+    vel_command.linear.x = 0.35;
+    vel_command.angular.z = ang_vel;
+}
+
+void vfh()
+{
+    std::vector<float> mag = calc_magnitude();
+    std::vector<int> hist_bin = calc_histogram(mag);
+    int valley_idx = select_sector(hist_bin);
+    vel_control(valley_idx);
+
+    // for debugging
+    std::cout << "cliff scan: " << cliff_range << std::endl;
+    std::cout << "histogram:\n";
+    for (int i = 0; i < hist_bin.size(); i++)
+        std::cout << hist_bin[i];
+    std::cout << "\nval_idx:" << valley_idx << " ang_vel:" << vel_command.angular.z << std::endl;
+}
+
+void view_lidar()
+{
+    std::cout << "START ANGLES" << std::endl;
+    for (int i = 0; i < lidar_angle.size(); i++)
+    {
+        std::cout << lidar_angle[i] << " ";
+    }
+    std::cout << "END ANGLES" << std::endl;
+
+    std::cout << "START RANGES" << std::endl;
+    for (int i = 0; i < lidar_range.size(); i++)
+    {
+        std::cout << lidar_range[i] << " ";
+    }
+    std::cout << "END RANGES" << std::endl;
+
+    std::cout << "total lasers:" << lidar_range.size() << std::endl;
+}
+
+void lidar_callback(const sensor_msgs::LaserScan::ConstPtr &msg)
+{
+    lidar_range = msg->ranges;
+    float beams = msg->angle_min;
+    while (beams <= msg->angle_max)
+    {
+        lidar_angle.push_back(beams);
+        beams += msg->angle_increment;
+    }
+    // view_lidar();
+}
+
+void cliff_callback(const sensor_msgs::LaserScan::ConstPtr &msg)
+{
+    cliff_range = msg->ranges.front();
+}
+
+void laser_odom_callback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+    float curr_x = msg->pose.pose.position.x;
+    float curr_y = msg->pose.pose.position.y;
+    float diff = sqrt((curr_x - prev_x) * (curr_x - prev_x) + (curr_y - prev_y) * (curr_y - prev_y));
+    std::cout << "dx:" << diff << std::endl;
+    if (diff <= 0.01) // robot is not moving
+        static_counter++;
+    else
+        static_counter = 0;
+
+    if (static_counter > 20)
+    {
+        std::cout << "getting stuck" << std::endl;
+        RECOVERY = 1;
+        static_counter = 0;
+    }
+    prev_x = curr_x;
+    prev_y = curr_y;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "husky_explore");
+
+    ros::NodeHandle n;
+    ros::Subscriber lidar_sub = n.subscribe("COSTAR_HUSKY/front_scan", 1, lidar_callback);
+    ros::Subscriber laser_odom_sub = n.subscribe("COSTAR_HUSKY/laser_odom", 1, laser_odom_callback);
+    ros::Subscriber cliff_sub = n.subscribe("COSTAR_HUSKY/front_cliff_scan", 1, cliff_callback);
+    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("COSTAR_HUSKY/cmd_vel", 10);
+
+    ROS_INFO("start exploration");
+
+    ros::Rate loop_rate(40);
+    int backward_counter = 0;
+    int spin_counter = 0;
+    std::cout << RECOVERY << std::endl;
+    while (n.ok())
+    {
+        ros::spinOnce();
+        if (RECOVERY == 0)
+        {
+            vfh();
+        }
+        else
+        {
+            // recovery behavior
+            if (backward_counter < 100) //50
+            {
+                vel_command.linear.x = -0.2;
+                vel_command.angular.z = 0.0;
+                backward_counter++;
+            }
+            else
+            {
+                if (spin_counter < 80)
+                {
+                    // use lidar distance to determine spin left or right
+                    float left_sum = 0;
+                    float right_sum = 0;
+                    for (auto beam = lidar_range.end(); beam > lidar_range.end() - lidar_range.size() / 2; beam--)
+                        left_sum += *beam;
+                    for (auto beam = lidar_range.begin(); beam < lidar_range.begin() + lidar_range.size() / 2; beam++)
+                        right_sum += *beam;
+                    // if (lidar_range.back() > lidar_range.front())
+                    if (left_sum >= right_sum)
+                        vel_command.angular.z = 0.3;
+                    else
+                        vel_command.angular.z = -0.3;
+                    vel_command.linear.x = 0.0;
+                    spin_counter++;
+                }
+                else
+                {
+                    backward_counter = 0;
+                    spin_counter = 0;
+                    RECOVERY = 0;
+                }
+            }
+        }
+        vel_pub.publish(vel_command);
+        loop_rate.sleep();
+    }
+
+    // stop robot
+    for (int i = 0; i < 10; i++)
+    {
+        vel_command.linear.x = 0.0;
+        vel_command.angular.z = 0.0;
+        vel_pub.publish(vel_command);
+    }
+
+    return 0;
+}
